ea8fc6599e7132e771284aa144ef0cb9f9b7627a
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-12-05 07:50:31 +0800

    fix: wrong execution of close method

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { app } from './app.js'
 const args = minimist(process.argv.slice(2))
 const PORT = args.port || 3000
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
 
@@ -14,6 +14,6 @@ if (import.meta.hot) {
   // @ts-ignore
   import.meta.hot.on('vite:beforeFullReload', async () => {
     console.info('Restarting server...')
-    await app.close()
+    await server.close()
   })
 }
