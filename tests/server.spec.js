6a363593add0b650cd4995f7c133e62b08fa6beb
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-12-05 05:37:18 +0800

    chore: lint fix

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,13 +1,13 @@
-import { describe, it } from 'vitest'
 import request from 'supertest'
+import { describe, it } from 'vitest'
 import { app } from '../src/server.js'
 
 describe('server', () => {
   it('should run', async () => {
-    app.get("/", (req, res) => {
+    app.get('/', (req, res) => {
       res.status(200).send()
     })
 
-    await request(app).get("/").expect(200)
+    await request(app).get('/').expect(200)
   })
 })
