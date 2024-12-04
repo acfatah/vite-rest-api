166710c096a3fb90e02a63da1617e0947976e4b9
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-12-05 07:29:20 +0800

    fix: vitest Serialized Error code: 'EADDRINUSE', errno: -98 when server instance already running on port 3000

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import compression from 'compression'
+import cors from 'cors'
+import express from 'express'
+import helmet from 'helmet'
+import httpStatus from 'http-status'
+
+import { errorConverter, errorHandler, logger } from './middlewares'
+import routes from './routes'
+import { ApiError } from './utils'
+
+export const app = express()
+
+app.use(logger)
+
+// set security HTTP headers
+app.use(helmet())
+
+// parse json request body
+app.use(express.json())
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }))
+
+// gzip compression
+app.use(compression())
+
+// enable cors
+app.use(cors())
+app.options('*', cors())
+
+// TODO: Add JWT authentication
+
+app.use(routes)
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'))
+})
+
+// convert error to ApiError, if needed
+app.use(errorConverter)
+
+// handle error
+app.use(errorHandler)
--- src/server.js
+++ src/server.js
@@ -1,62 +1,19 @@
 import process from 'node:process'
-import compression from 'compression'
-import cors from 'cors'
-import express from 'express'
-import helmet from 'helmet'
-import httpStatus from 'http-status'
 import minimist from 'minimist'
-
-import { errorConverter, errorHandler, logger } from './middlewares'
-import routes from './routes'
-import { ApiError } from './utils'
+import { app } from './app.js'
 
 const args = minimist(process.argv.slice(2))
 const PORT = args.port || 3000
 
-export const app = express()
-
-app.use(logger)
-
-// set security HTTP headers
-app.use(helmet())
-
-// parse json request body
-app.use(express.json())
-
-// parse urlencoded request body
-app.use(express.urlencoded({ extended: true }))
-
-// gzip compression
-app.use(compression())
-
-// enable cors
-app.use(cors())
-app.options('*', cors())
-
-// TODO: Add JWT authentication
-
-app.use(routes)
-
-// send back a 404 error for any unknown api request
-app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'))
-})
-
-// convert error to ApiError, if needed
-app.use(errorConverter)
-
-// handle error
-app.use(errorHandler)
-
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
 
 // @ts-ignore
 if (import.meta.hot) {
   // @ts-ignore
-  import.meta.hot.on('vite:beforeFullReload', () => {
+  import.meta.hot.on('vite:beforeFullReload', async () => {
     console.info('Restarting server...')
-    server.close()
+    await app.close()
   })
 }
