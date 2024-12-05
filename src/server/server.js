b28774d9871dc00530a07136e8e5efc71ac6b4f4
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-12-06 05:56:20 +0800

    Add host information

--- src/server/server.js
+++ src/server/server.js
@@ -6,15 +6,21 @@ const args = minimist(process.argv.slice(2))
 const PORT = args.port || 3000
 const MODE = process.env.NODE_ENV || 'development'
 
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} in ${MODE} mode`)
+const server = app.listen(PORT)
+
+server.on('listening', () => {
+  // @ts-ignore
+  const { address, port } = server.address()
+  const host = address === '::' ? 'localhost' : address
+
+  console.log(`Server is listening on ${host}:${port} in ${MODE} mode`)
 })
 
 // @ts-ignore
 if (import.meta.hot) {
   // @ts-ignore
-  import.meta.hot.on('vite:beforeFullReload', async () => {
-    console.info('Restarting server...')
-    await server.close()
+  import.meta.hot.on('vite:beforeFullReload', () => {
+    console.log('Restarting server...')
+    server.close()
   })
 }
