5a278b3c07da63a8feac34422a3dbb1f19cc1555
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-12-06 09:23:29 +0800

    Update Vite to load dotenv file based on mode

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,33 @@
 import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
+import process from 'node:process'
+import dotenv from 'dotenv'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-      output: {
-        dir: 'dist',
+export default defineConfig(({ mode }) => {
+  loadEnv(mode, process.cwd())
+
+  if (mode === 'production') {
+    dotenv.config({ path: '.env' })
+  }
+  else if (mode === 'test') {
+    dotenv.config({ path: '.env.test' })
+  }
+  else {
+    dotenv.config({ path: '.env.development' })
+  }
+
+  return {
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+        },
+        output: {
+          dir: 'dist',
+        },
       },
     },
-  },
-  publicDir: 'public',
+    publicDir: 'public',
+  }
 })
